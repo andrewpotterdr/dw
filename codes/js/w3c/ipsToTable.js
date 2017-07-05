3b181a492d027f3014bbc7d0121a4e85ce662026
Luiz Carlos <lucachaves@gmail.com>
2017-07-05 04:55:20 -0300

    refatorando ipsToTable.js

---
+++ codes/js/w3c/ipsToTable.js
@@ -0,0 +1,28 @@
+class IpTables {
+
+  constructor(ips) {
+    this.ips = ips
+  }
+
+  toHTML() {
+    let convertToRow = (tds) => `<tr><td>${tds.join('</td><td>')}</td></tr>`
+    let ipInfo = (ip) => convertToRow(Object.values(ip))
+    return this.ips.map(ipInfo).join('')
+  }
+
+  render() {
+    const ipTbody = document.querySelector('#iptable table tbody')
+    ipTbody.innerHTML = this.toHTML()
+  }
+
+}
+
+let ips = [
+  {ip: '192.168.0.1', mask: '255.255.255.0', version: 'v4'},
+  {ip: '192.168.0.5', mask: '255.255.255.0', version: 'v4'},
+  {ip: '192.168.0.7', mask: '255.255.255.0', version: 'v4'},
+  {ip: '192.168.0.10', mask: '255.255.255.0', version: 'v4'}
+]
+
+let iptables = new IpTables(ips)
+iptables.render()
