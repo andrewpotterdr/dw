bd0c51839d7d8cce9fae952a833a556cbb74ff33
Luiz Carlos <lucachaves@gmail.com>
2017-07-12 05:47:49 -0300

    js: ipInfo

--- codes/js/w3c/ipInfo.js
+++ codes/js/w3c/ipInfo.js
@@ -1,10 +1,37 @@
 
-let ip = '8.8.8.8'
-let url = `https://ipinfo.io/${ip}/json`
+class Address {
+  constructor(info, table) {
+    this.info = info
+    this.table = table
+    this.render()
+  }
 
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    console.log(data.city)
-  })
+  render() {
+    let infoTable = '<table>'
+    for(let [key,value] of Object.entries(this.info)) {
+        infoTable+= `<tr> <td>${key}</td> <td>${value}</td> </tr>`
+    }
+    infoTable+='</table>'
+    this.table.innerHTML = infoTable
+  }
+}
+
+const ipAddressInput = document.querySelector('#ip-addres')
+const getIpButton = document.querySelector('#get-ip')
+const addressDiv = document.querySelector('#address-table')
+
+getIpButton.addEventListener('click',function() {
+
+  let ip = ipAddressInput.value
+  let url = `https://ipinfo.io/${ip}/json`
+
+  addressDiv.innerHTML = '<img src="loading.gif">'
+
+  fetch(url)
+    .then(res => res.json())
+    .then(info => {
+      console.log(info)
+      let address = new Address(info, addressDiv)
+    })
+
+})
